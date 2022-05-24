3a5a44aa2a9b0ab23b5f0849c05b58497666d59c
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-24 07:53:44 +0200

    Create theme icon

---
+++ src/app/svgs/theme-icon/theme-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-icon',
+  templateUrl: './theme-icon.component.html',
+  styleUrls: ['./theme-icon.component.scss'],
+})
+export class ThemeIconComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
