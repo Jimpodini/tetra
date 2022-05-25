2a08849aa0dfcc6452b247d1176cf601b457523a
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-05-25 07:44:00 +0200

    Add support for themes and dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostBinding,
+  ViewChild,
+} from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
+import { ThemeService } from './services/theme.service';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Subscription } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +17,48 @@ import { MediaMatcher } from '@angular/cdk/layout';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @HostBinding('class.darkMode') darkMode: boolean = false;
   mobileQuery: MediaQueryList;
+  themeSubscription: Subscription;
+
+  @ViewChild(MatSidenav, { read: ElementRef }) matSidenav:
+    | ElementRef
+    | undefined;
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    public themeService: ThemeService,
+    overlayContainer: OverlayContainer
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+
+    this.themeSubscription = themeService.themeObservable$.subscribe(
+      (theme) => {
+        if (theme === 'dark') {
+          this.darkMode = true;
+          console.log(theme);
+          overlayContainer.getContainerElement().classList.add('darkMode');
+          this.matSidenav?.nativeElement.classList.remove('primary');
+        } else {
+          if (theme === 'primary') {
+            this.matSidenav?.nativeElement.classList.add('primary');
+          } else {
+            this.matSidenav?.nativeElement.classList.remove('primary');
+          }
+          this.darkMode = false;
+          overlayContainer.getContainerElement().classList.remove('darkMode');
+        }
+      }
+    );
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+    this.themeSubscription.unsubscribe();
   }
 }
