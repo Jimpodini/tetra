fde9fc495d0441de605b2a4ca8775355c5c724c9
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-06-06 08:58:16 +0200

    cms-box - detect click outside text area

--- src/app/components/cms-box/cms-box.component.ts
+++ src/app/components/cms-box/cms-box.component.ts
@@ -1,15 +1,40 @@
-import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnDestroy,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cms-box',
   templateUrl: './cms-box.component.html',
   styleUrls: ['./cms-box.component.scss'],
 })
-export class CmsBoxComponent {
-  @HostListener('click') onMouseOver() {
+export class CmsBoxComponent implements OnDestroy {
+  listenerFunction: () => void;
+  @ViewChild('textArea')
+  text: ElementRef | undefined;
+  @HostListener('click') onClick() {
     this.editMode = true;
   }
   @HostBinding('class.editMode') editMode = false;
 
-  constructor() {}
+  constructor(private renderer: Renderer2) {
+    this.listenerFunction = this.renderer.listen(
+      'window',
+      'click',
+      (e: Event) => {
+        if (e.target !== this.text?.nativeElement) {
+          this.editMode = false;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.listenerFunction();
+  }
 }
