8b196aaa28bb88a263ebac63e99bf6d415466aeb
Jim Lindberg <jim.erik.lindberg@gmail.com>
2022-06-18 13:26:01 +0200

    Remove unused ngOnInit

--- src/app/toolbar-icons/toolbar-icons.component.ts
+++ src/app/toolbar-icons/toolbar-icons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ThemeService } from '../services/theme.service';
 
 @Component({
@@ -6,8 +6,6 @@ import { ThemeService } from '../services/theme.service';
   templateUrl: './toolbar-icons.component.html',
   styleUrls: ['./toolbar-icons.component.scss'],
 })
-export class ToolbarIconsComponent implements OnInit {
+export class ToolbarIconsComponent {
   constructor(public themeService: ThemeService) {}
-
-  ngOnInit(): void {}
 }
